f37c92f02d753d27cb8b3851f67470cf067d499c
oriofir <97814056+oriofir@users.noreply.github.com>
2023-02-24 17:54:27 -0800

    further developed the schema

--- models/invoice-model.js
+++ models/invoice-model.js
@@ -9,16 +9,41 @@ const InvoiceSchema = new mongoose.Schema({
     type: Date,
     required: true,
   },
+  nameFrom: {
+    type: String,
+    required: true,
+  },
+  emailFrom: {
+    type: String,
+    required: true,
+  },
+  addressFrom: {
+    type: String,
+    required: true,
+  },
+  phoneNumberFrom: {
+    type: Number,
+    required: false,
+  },
   nameTo: {
     type: String,
     required: true,
   },
-  email: {
+  emailTo: {
     type: String,
     required: true,
   },
-  address: {
+  addressTo: {
     type: String,
     required: true,
+  },
+  phoneNumberTo: {
+    type: Number,
+    required: false,
+  },
+
+  terms: {
+    type: String,
+    required: false,
   },
 });
