cbffc251acae1f56f6b28f370be6a8be470d84ca
oriofir <97814056+oriofir@users.noreply.github.com>
2023-02-24 17:49:24 -0800

    added to the invoice schema

--- models/invoice-model.js
+++ models/invoice-model.js
@@ -1,1 +1,24 @@
 const mongoose = require("../db/connection");
+
+const InvoiceSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: false,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  nameTo: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
